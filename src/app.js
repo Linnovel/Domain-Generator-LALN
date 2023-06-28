9bfeab60c7c1a33c714cf4492e21f52e4fbf9595
Linnovel <luislinnovel@gmail.com>
2023-06-28 01:35:27 +0000

    Domain G Done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "My"];
+  let adjs = ["great", "big", "Llanero"];
+  let nouns = ["jogger", "racoon", "Capybara"];
+  let domains = [".com", ".org", ".us", ".ve"];
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          console.log(`${pronoun}${adj}${noun}${domain}`);
+        }
+      }
+    }
+  }
 };
